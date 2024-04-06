587c04a8dbae9178000405e8f5be8cb8fa411f0d
MattJurkowski <jurek198@gmail.com>
2024-04-06 09:46:22 +0000

    lab 1 os error
    Signed-off-by: MattJurkowski <jurek198@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include "mbed.h"
+#include <cstudio>
 
+InterruptIn buttn(PC_13)
 
-//just putting in a comment to show a second commit and push
+void buttn_fall(){
+    printf("button pressed!!\r\n")
+}
 
-// main() runs in its own thread in the OS
+//main() runs in its own thread in the OS
 int main()
 {
+    printf("This is Mbed os V%d \r\n", MBED_VERSION)
+
+    button.fall(&buttn_fall);
     while (true) {
     }
 }
