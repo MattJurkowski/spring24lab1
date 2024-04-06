129fd2c97fccfa31d16070597f980fdd26f9e7a0
MattJurkowski <jurek198@gmail.com>
2024-04-06 09:59:35 +0000

    program with error
    Signed-off-by: MattJurkowski <jurek198@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,19 +1,20 @@
 #include "mbed.h"
-#include <cstudio>
+#include <cstdio>
 
 InterruptIn buttn(PC_13)
 
-void buttn_fall(){
-    printf("button pressed!!\r\n")
+void button_fall(){
+    printf("button pressed!!!\r\n");
 }
 
 //main() runs in its own thread in the OS
 int main()
 {
-    printf("This is Mbed os V%d \r\n", MBED_VERSION)
+    printf("This is Mbed os V%d \r\n", MBED_VERSION);
 
-    button.fall(&buttn_fall);
+    buttn.fall(&buttn_fall);
     while (true) {
     }
+       
 }
 
